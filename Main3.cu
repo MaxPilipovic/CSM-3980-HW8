15f55a388cc3ef57db90df3cf55a770c54be7efc
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-04-11 12:46:45 -0500

    Update Main3.cu

--- Main3.cu
+++ Main3.cu
@@ -8,11 +8,11 @@ __global__ void factorablequadratics_kernel(int* count, int i) {
 
     for (int start = index; start < 2 * i * i * i; start += stride) {
         int a = (start / ((2 * i + 1) * (2 * i + 1)));
-        printf("A VALUE %d\n", b);
+        printf("A VALUE %d\n", a);
         int b = (start / ((2 * i + 1) % (2 * i + 1)));
         printf("B VALUE %d\n", b);
         int c = (start % (2 * i + 1));
-        printf("C VALUE %d\n", b);
+        printf("C VALUE %d\n", c);
         
         if (a != 0 && b != 0 && c != 0) {
             int check = b * b - 4 * a * c;
