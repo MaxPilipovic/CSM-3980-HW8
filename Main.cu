8579e172919f3ba841768f3fd3a4a066f958bf3e
MaxPilipovic <maxpkusa@gmail.com>
2024-04-11 13:41:56 -0700

    - MAIN1 TEST AND MAIN2 SKELETON

--- Main.cu
+++ Main.cu
@@ -1,5 +1,44 @@
-//
-// Created by Max on 4/9/2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void readFile(char* filename, int freqLetters) {
+    char ch;
 
-#include "Main.cuh"
+    FILE* read = fopen(filename, "r");
+    if(read != NULL) {
+        for (int i = 0; i < 26; i++) {
+            freqLetters[0];
+        }
+        while ((ch = fgetc(filename)) != EOF) {
+            ch = toupper(ch);
+            if (ch >= 'A' && ch <= 'Z') {
+                freqLetters[ch - 'A']++;
+            }
+        }
+        fclose(read);
+    } else {
+        printf("Nothing in file");
+        exit(1);
+    }
+}
+
+void printA(int freqLetters[]) {
+    for (int i = 0; i < 26; i++) {
+        printf(freqLetters[i]);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Giv me file name");
+        return 0;
+    }
+
+    int n
+    int letterFreq[26]
+    readFile(argv[1], freqLetters);
+    printf("Printing frequencies in alphabetical order\n");
+    printA(letterFreq);
+
+    return 0;
+}
--- Main2.cu
+++ Main2.cu
@@ -1,5 +1,52 @@
-//
-// Created by Max on 4/9/2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include "Main2.cuh"
+__global__ void textfrequencycounter_kernel() {
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+}
+
+void textfrequencycounter() {
+
+    //Allocate GPU memory
+
+    //Copy data to GPU memory
+
+
+    //Start time
+    float time;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+
+    //Perform computation on GPU
+
+    //Synchronize
+    cudaDeviceSynchronize();
+
+    //End time
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    printf("%f\n", time);
+
+    //Copy data from GPU memory
+
+
+    //Deallocate GPU memory
+
+}
+
+
+int main() {
+
+    //PERFORM TEXT FREQUENCY COUNTER
+
+    //FREE
+
+
+    return 0;
+}
