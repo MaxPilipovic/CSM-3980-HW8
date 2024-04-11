27eb4a7e1dfb0ae64280d9a6e2110162d8f60527
MaxPilipovic <maxpkusa@gmail.com>
2024-04-11 15:40:46 -0700

    - MAIN1 TEST AND MAIN2 SKELETON

--- Main.cu
+++ Main.cu
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void readFile(char* filename, int freqLetters) {
+#include <ctype.h>
+
+void readFile(char* filename, int *freqLetters) {
     char ch;
 
     FILE* read = fopen(filename, "r");
@@ -9,7 +11,7 @@ void readFile(char* filename, int freqLetters) {
         for (int i = 0; i < 26; i++) {
             freqLetters[0];
         }
-        while ((ch = fgetc(filename)) != EOF) {
+        while ((ch = fgetc(read)) != EOF) {
             ch = toupper(ch);
             if (ch >= 'A' && ch <= 'Z') {
                 freqLetters[ch - 'A']++;
@@ -24,7 +26,7 @@ void readFile(char* filename, int freqLetters) {
 
 void printA(int freqLetters[]) {
     for (int i = 0; i < 26; i++) {
-        printf(freqLetters[i]);
+        printf("%d ", freqLetters[i]);
     }
 }
 
@@ -34,8 +36,8 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    int n
-    int letterFreq[26]
+    //int n;
+    int letterFreq[26];
     readFile(argv[1], freqLetters);
     printf("Printing frequencies in alphabetical order\n");
     printA(letterFreq);
