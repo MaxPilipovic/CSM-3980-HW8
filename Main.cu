08124f35ec6e26a5e94fde04da2241541e9646ff
MaxPilipovic <maxpkusa@gmail.com>
2024-04-11 17:42:54 -0700

    - IMPLEMENTED DIGRAM AND TRIGRAM

--- Main.cu
+++ Main.cu
@@ -3,18 +3,32 @@
 #include <time.h>
 #include <ctype.h>
 
-void readFile(char* filename, int *freqLetters) {
+void readFile(char* filename, int *letterFreq), int *digramFreq, int *trigramFreq) {
     char ch;
+    char prev;
+    char prev2;
 
     FILE* read = fopen(filename, "r");
     if(read != NULL) {
         for (int i = 0; i < 26; i++) {
-            freqLetters[i] = 0;
+            letterFreq[i] = 0;
+            int (j = 0; j < 26; j++) {
+                digramFreq[i][j];
+                int (k = 0; k < 26; k++) {
+                    trigramFreq[i][j][k];
+                }
+            }
         }
         while ((ch = fgetc(read)) != EOF) {
             ch = toupper(ch);
             if (ch >= 'A' && ch <= 'Z') {
-                freqLetters[ch - 'A']++;
+                letterFreq[ch - 'A']++;
+                if (prev >= 'A' && prev <= 'Z') {
+                    digramFreq[prev - 'A'][ch - 'A']++;
+                    if (prev2 >= 'A' && prev2 <= 'Z') {
+                        trigramFreq[prev2 - 'A'][prev - 'A'][ch - 'A'];
+                    }
+                }
             }
         }
         fclose(read);
@@ -24,9 +38,9 @@ void readFile(char* filename, int *freqLetters) {
     }
 }
 
-void printA(int freqLetters[]) {
+void printA(int letterFreq[]) {
     for (int i = 0; i < 26; i++) {
-        printf("%c - %d ", 'A' + i, freqLetters[i]);
+        printf("%c - %d ", 'A' + i, letterFreq[i]);
     }
 }
 
@@ -61,6 +75,28 @@ void printR(int letterFreq[]) {
     }
 }
 
+void printD(int letterFreq) {
+    if (i = 0; i < 26; i++) {
+        if (j = 0; j < 26; j++) {
+            if (digramFreq > 0) {
+                printf("%c - %d ",'A' - i, 'A' - j, digramFreq[i][j]);
+            }
+        }
+    }
+}
+
+void printT(int letterFreq) {
+    if (i = 0; i < 26; i++) {
+        if (j = 0; j < 26; j++) {
+            if (k = 0; k < 26; k++) {
+                if (trigramFreq > 0) {
+                    printf("%c - %d ",'A' - i, 'A' - j, 'A' - k, trigramFreq[i][j][k]);
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Giv me file name");
@@ -69,15 +105,26 @@ int main(int argc, char *argv[]) {
 
     //int N;
     int letterFreq[26];
+    int digramFreq[26][26];
+    int trigramFreq[26][26][26]
+    ;
     readFile(argv[1], letterFreq);
 
     printf("Printing frequencies in alphabetical order\n");
     printA(letterFreq);
-
-    printf("\n");
+    printf("-----\n");
 
     printf("Printing frequencies in rank order\n");
     printR(letterFreq);
+    printf("-----\n");
+
+    printf("Digram frequencies\n");
+    printD(letterFreq);
+    printf("-----\n");
+
+    printf("Trigram frequencies\n");
+    printT(letterFreq);
+    printf("-----\n");
 
     return 0;
 }
