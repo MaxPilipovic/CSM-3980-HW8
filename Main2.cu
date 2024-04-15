be89ef7736778a0f63a14a529d92f1bc3e916a6c
MaxPilipovic <maxpkusa@gmail.com>
2024-04-15 11:19:49 -0700

    - MAIN 2 UPDATE

--- Main2.cu
+++ Main2.cu
@@ -3,50 +3,123 @@
 #include <time.h>
 
 __global__ void textfrequencycounter_kernel() {
-    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    // Private copies of the result for each block
+    __shared__ int privateResult[26];
+
+    // Initialize private result
+    if(threadIdx.x < 26)
+    {
+        privateResult[threadIdx.x] = 0;
+    }
+
+    __syncthreads();
+
+    // Get starting location and stride. Striding this way gives
+    // coalesced memory access
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = blockDim.x * gridDim.x;
 
+    while(i < n)
+    {
+        // Handle uppercase
+        int resultIndex = s[i] - 'A';
+        if(resultIndex >= 0 && resultIndex < 26)
+        {
+            atomicAdd(&(privateResult[resultIndex]), 1);
+        }
+
+        // Handle lowercase
+        resultIndex = s[i] - 'a';
+        if(resultIndex >= 0 && resultIndex < 26)
+        {
+            atomicAdd(&(privateResult[resultIndex]), 1);
+        }
+
+        i+=stride;
+    }
+
+    __syncthreads();
+
+    // Store final result
+    if(threadIdx.x < 26)
+    {
+        atomicAdd(&(result[threadIdx.x]), privateResult[threadIdx.x]);
+    }
 }
 
-void textfrequencycounter() {
+int main(int argc, const char* argv[])
+{
+    // Check that a file name was given
+    if(argc < 2)
+    {
+        fprintf(stderr, "No filename given!\n");
+        return 1;
+    }
 
-    //Allocate GPU memory
+    // Open file
+    FILE *file = fopen(argv[1], "r");
+    if(!file)
+    {
+        fprintf(stderr, "Unable to open %s!\n", argv[1]);
+        return 2;
+    }
 
-    //Copy data to GPU memory
+    // Find file size.
+    //
+    // Note I should check the return values from fseek, but I am going to
+    // skip error checking for this and other functions from now on i.e.
+    // do not do this in code you ship.
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
+    // Arrays to hold our data on the host and gpu
+    unsigned char *hostData = (unsigned char*) malloc((size + 1) * sizeof(char));
+    unsigned char *gpuData;
+    cudaMalloc(&gpuData, (size + 1) * sizeof(char));
 
-    //Start time
-    float time;
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start, 0);
+    // Read data (adding null)
+    printf("Reading %s\n", argv[1]);
+    fread(hostData, size, 1, file);
+    fclose(file);
+    hostData[size] = '\0';
 
-    //Perform computation on GPU
+    // Allocate arrays to hold our results
+    int *result;
+    cudaMallocManaged(&result, 26*sizeof(int));
 
-    //Synchronize
+    // Set the size of our grid. Threads must be >= 256.
+    int threads = 256;
+    int blocks = 80;
+
+    // Copy data to GPU. I am not sure if it is worth keeping the
+    // null at the end of the text as it is not used as a terminator
+    // in our kernels, but I was taught to always terminate my strings...
+    cudaMemcpy(gpuData, hostData, (size + 1) * sizeof(char), cudaMemcpyHostToDevice);
+    int device = -1;
+    cudaGetDevice(&device);
+    cudaMemPrefetchAsync(result, 26 * sizeof(int), device);
+
+    // Run histograms
+    textfrequencycounter_kernel<<<blocks,threads>>>(gpuData, result, size);
+
+    // Move results back to host
+    cudaMemPrefetchAsync(result, 26 * sizeof(int), cudaCpuDeviceId);
+
     cudaDeviceSynchronize();
 
-    //End time
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&time, start, stop);
-    printf("%f\n", time);
+    // Print results
+    for(int index = 0; index < 26; index++)
+    {
+        printf("%c : %5d : %5d : %5d\n",
+               (char)('A' + index),
+               result[index],
+    }
 
-    //Copy data from GPU memory
-
-
-    //Deallocate GPU memory
-
-}
-
-
-int main() {
-
-    //PERFORM TEXT FREQUENCY COUNTER
-
-    //FREE
-
+    // Clean up
+    free(hostData);
+    cudaFree(gpuData);
+    cudaFree(result);
 
     return 0;
 }
